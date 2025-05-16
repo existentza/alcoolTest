ba1861135771b4fb54062dee0990e31c90ae0071
existentza <158862479+existentza@users.noreply.github.com>
2025-05-16 17:00:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,127 +1,151 @@
-    // HTML
-    const resultDisplay = document.getElementById("result");
-    const alcoolUnitsContainer = [...document.getElementById("alcool-units").children];
-    const selectedUnitLabel = document.getElementById("selectedUnitLabel");
-    const selectedUnitAmount = document.getElementById("selectedUnitAmount");
-    const bodyWeightInput = document.getElementById("body-weight");
-    const sexSelection = document.getElementById("gender");
-    const stomachSelection = document.getElementById("stomach");
-    const consumeTimeAdvice = document.getElementById("consumeTime");
+// HTML
+const resultDisplay = document.getElementById("result");
+const resultText = document.getElementById("resultText");
+const alcoolUnitsContainer = document.getElementById("alcool-units");
+const selectedUnitLabel = document.getElementById("selectedUnitLabel");
+const selectedUnitAmount = document.getElementById("selectedUnitAmount");
+const bodyWeightInput = document.getElementById("body-weight");
+const sexSelection = document.getElementById("gender");
+const stomachSelection = document.getElementById("stomach");
+const timePassedInput = document.getElementById("time");
 
-    // VARS
-    const alcoolUnits = {
-        "birra analcolica": [0.5, 330],
-        "birra leggera": [3.5, 330],
-        "birra normale": [5, 330],
-        "birra speciale": [8, 330],
-        "birra doppio malto": [10, 330],
-        "vino": [12, 125],
-        "vini liquorosi-aperitivi": [18, 80],
-        "digestivi": [25, 40],
-        "superalcolici": [35, 40],
-        "bollicine": [11, 100],
-        "ready to drink": [2.8, 150],
+// VARS
+const alcoolUnits = {
+    "Birra Analcolica": [0.5, 330],
+    "Birra Leggera": [3.5, 330],
+    "Birra Normale": [5, 330],
+    "Birra Speciale": [8, 330],
+    "Birra Doppio Malto": [10, 330],
+    "Vino": [12, 125],
+    "Vini Liquorosi-Aperitivi": [18, 80],
+    "Digestivi": [25, 40],
+    "Superalcolici": [35, 40],
+    "Bollicine": [11, 100],
+    "Ready to Drink": [2.8, 150],
+}
+
+let consumedUnits = {
+    "Birra Analcolica": 0,
+    "Birra Leggera": 0,
+    "Birra Normale": 0,
+    "Birra Speciale": 0,
+    "Birra Doppio Malto": 0,
+    "Vino": 0,
+    "Vini Liquorosi-Aperitivi": 0,
+    "Digestivi": 0,
+    "Superalcolici": 0,
+    "Bollicine": 0,
+    "Ready to Drink": 0,
+}
+
+let absorptionRates = {
+    "male": 0.68,
+    "female": 0.55,
+}
+
+// Starting Unit
+let selectedUnit = "Birra Analcolica";
+let sex = "male";
+let stomach = "full"; 
+
+let bodyWeight = 0; // Kg
+let timePassed = 0; // Minuti
+let alcoolConsumeRate = 0.15 // g/L (tasso di consumo alcolico del corpo umano)
+
+Object.entries(alcoolUnits).forEach(([name, [graduation, ml]]) => {
+    const button = document.createElement("button");
+    button.classList.add("unit");
+    button.innerText = `${name} (${graduation} % - ${ml} ml)`;
+    
+    button.dataset.name = name;
+
+    if (name === selectedUnit){
+        button.id = "select";   
     }
 
-    let consumedUnits = {
-        "birra analcolica": 0,
-        "birra leggera": 0,
-        "birra normale": 0,
-        "birra speciale": 0,
-        "birra doppio malto": 0,
-        "vino": 0,
-        "vini liquorosi-aperitivi": 0,
-        "digestivi": 0,
-        "superalcolici": 0,
-        "bollicine": 0,
-        "ready to drink": 0,
-    }
+    button.onclick = function () {
+        selectUnit(button);
+    };
 
-    let absorptionRates = {
-        "male": 0.68,
-        "female": 0.55,
-    }
+    alcoolUnitsContainer.appendChild(button);
+});
 
-    // Starting Unit
-    let selectedUnit = "birra analcolica";
-    let sex = "male";
-    let stomach = "full"; 
-
-    let bodyWeight = 0; // Kg
-    let timePassed = 100; // Minuti
-    let alcoolConsumeRate = 0.15 // g/L (tasso di consumo alcolico del corpo umano)
-
-    consumeTimeAdvice.innerHTML = `Il calcolo tiene in considerazione che l'alcool sia stato consumato ${timePassed} minuti fà.`
-
-    function calculateAlcool(){
-        let totalAlcool = 0;
-        for (const consumedUnitName in consumedUnits){
-            let consumedUnit = consumedUnits[consumedUnitName];
-            
-            if (consumedUnit <= 0) {
-                continue
-            }
-
-            console.log(`UNITA CONSUMATE: ${consumedUnit}`);
-
-            if (alcoolUnits.hasOwnProperty(consumedUnitName)){
-                let alcoolUnit = alcoolUnits[consumedUnitName];
-
-                let consumedQuantity = (alcoolUnit[1] * consumedUnit);
-                let alcoolGraduation = alcoolUnit[0];
-                let pureAlcool = consumedQuantity * (alcoolGraduation / 100);
-
-                let unitTotalAlcool = pureAlcool / (bodyWeight * absorptionRates[sex]);
-                let timeReduction = alcoolConsumeRate * (timePassed / 60);
-                let effectiveTotalAlcool = unitTotalAlcool - timeReduction;
-
-                totalAlcool += effectiveTotalAlcool;
-                console.log(`TOTAL ALCOOL: ${effectiveTotalAlcool}`);
-            }
-        };
-
-        if (totalAlcool < 0){
-            totalAlcool = 0;
+function calculateAlcool(){
+    let totalAlcool = 0;
+    for (const consumedUnitName in consumedUnits){
+        let consumedUnit = consumedUnits[consumedUnitName];
+        
+        if (consumedUnit <= 0) {
+            continue
         }
 
-        return totalAlcool
-    }
+        console.log(`UNITA CONSUMATE: ${consumedUnit}`);
 
-    function selectUnit(unit){
-        alcoolUnitsContainer.forEach(element => {
-            element.id = "";
-        }); 
-        unit.id = "select";
-        selectedUnit = unit.innerHTML;
-        selectedUnitLabel.innerHTML = selectedUnit + " consumate: ";
-        selectedUnitAmount.value = consumedUnits[selectedUnit];
-    }
+        if (alcoolUnits.hasOwnProperty(consumedUnitName)){
+            let alcoolUnit = alcoolUnits[consumedUnitName];
 
-    alcoolUnitsContainer.forEach(element => {
-        element.onclick = function(){
-            selectUnit(element);
+            let consumedQuantity = (alcoolUnit[1] * consumedUnit);
+            let alcoolGraduation = alcoolUnit[0];
+            let pureAlcool = consumedQuantity * (alcoolGraduation / 100);
+
+            let unitTotalAlcool = (pureAlcool / (bodyWeight * absorptionRates[sex])) * (stomach === "full" ? 1 : 1.3);
+            let timeReduction = (timePassed > 0 ? alcoolConsumeRate * (timePassed / 60) : 0);
+            let effectiveTotalAlcool = unitTotalAlcool - timeReduction;
+
+            totalAlcool += effectiveTotalAlcool;
+            console.log(`TOTAL ALCOOL: ${effectiveTotalAlcool}`);
         }
-    });
+    };
 
-    selectedUnitAmount.onchange = function(){
-        consumedUnits[selectedUnit] = selectedUnitAmount.value;
-        resultDisplay.innerHTML = `${calculateAlcool()} g/L`
+    if (totalAlcool < 0){
+        totalAlcool = 0;
     }
 
-    bodyWeightInput.onchange = function(){
-        bodyWeight = bodyWeightInput.value;
-        console.log(`WEIGHT: ${bodyWeight}`);
-        resultDisplay.innerHTML = `${calculateAlcool()} g/L`
+    if (totalAlcool > 0.5){ 
+        resultDisplay.className = "result-negative";
+        resultText.innerHTML = "Sei oltre il limite di legge, non mettere in pericolo la tua vita e quella degli altri!";
+    } else {
+        resultDisplay.className = "result-positive";
+        resultText.innerHTML = "Puoi metterti alla guida se non sei neopatentato.";
     }
 
-    sexSelection.addEventListener("change", function() {
-        sex = this.value; 
-        console.log(`SEX: ${sex}`)
-        resultDisplay.innerHTML = `${calculateAlcool()} g/L`
-    });
+    return totalAlcool
+}
 
-    stomachSelection.addEventListener("change", function() {
-        stomach = this.value; 
-        console.log(`STOMACH: ${stomach}`)
-        resultDisplay.innerHTML = `${calculateAlcool()} g/L`
-    });
+function selectUnit(unit){
+    document.querySelectorAll(".unit").forEach(el => el.id = "");
+    unit.id = "select";
+
+    selectedUnit = unit.dataset.name;
+    selectedUnitLabel.innerHTML = `${selectedUnit} consumate:`;
+    selectedUnitAmount.value = consumedUnits[selectedUnit];
+}
+
+selectedUnitAmount.onchange = function(){
+    consumedUnits[selectedUnit] = selectedUnitAmount.value;
+    resultDisplay.innerHTML = `${calculateAlcool()} g/L`
+}
+
+bodyWeightInput.onchange = function(){
+    bodyWeight = bodyWeightInput.value;
+    console.log(`WEIGHT: ${bodyWeight}`);
+    resultDisplay.innerHTML = `${calculateAlcool()} g/L`
+}
+
+sexSelection.addEventListener("change", function() {
+    sex = this.value; 
+    console.log(`SEX: ${sex}`)
+    resultDisplay.innerHTML = `${calculateAlcool()} g/L`
+});
+
+stomachSelection.addEventListener("change", function() {
+    stomach = this.value; 
+    console.log(`STOMACH: ${stomach}`)
+    resultDisplay.innerHTML = `${calculateAlcool()} g/L`
+});
+
+timePassedInput.onchange = function(){
+    timePassed = timePassedInput.value;
+    console.log(`TIME PASSED: ${timePassed}`);
+    resultDisplay.innerHTML = `${calculateAlcool()} g/L`
+}
